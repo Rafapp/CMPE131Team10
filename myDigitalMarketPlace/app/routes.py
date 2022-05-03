b5e89692407bc6a3f305219a6c8d45b8f47e0bf8
Rafa <rpadiper@gmail.com>
2022-05-02 21:48:24 -0700

    Created home page, working on sign up and log in
    - Created the entire home page website using bootstrap, and edited template
    - Connected to flask
    - Started work on sign up / log in page

--- myDigitalMarketPlace/app/routes.py
+++ myDigitalMarketPlace/app/routes.py
@@ -1,6 +1,7 @@
 from app import flaskObj
 from flask import render_template
 
+<<<<<<< Updated upstream
 # Splash page (Umesh)
 @flaskObj.route('splash')
 def splash():
@@ -45,3 +46,49 @@ def seller():
 @flaskObj.route('rating')
 def cart():
     return 'home'
+=======
+# Home page (Rafael) DONE!
+@flaskObj.route('/')
+def home():
+    return render_template("Home.html")
+
+# Splash page (Umesh)
+@flaskObj.route('/splash')
+def splash():
+    return 'splash'
+
+# Log in (Rafael)
+@flaskObj.route('/login')
+def login():
+    return 'login'
+
+# Sign up (Rafael)
+@flaskObj.route('/signup')
+def signup():
+    return render_template("Signup.html")
+
+# Profile (Rafael)
+@flaskObj.route('/profile')
+def profile():
+    return 'profile'
+
+# Cart (Mohammad)
+@flaskObj.route('/cart')
+def cart():
+    return 'cart'
+
+# Item Search (Mohammad)
+@flaskObj.route('/itemsearch')
+def itemearch():
+    return 'itemsearch'
+
+# Item seller (Umesh)
+@flaskObj.route('/seller')
+def seller():
+    return 'seller'
+
+# Item rating (Umesh)
+@flaskObj.route('/rating')
+def rating():
+    return 'rating'
+>>>>>>> Stashed changes
