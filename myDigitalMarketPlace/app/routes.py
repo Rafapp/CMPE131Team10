178b49e57eac5ca64465bfb84806ae2f6f08a7ea
Rafa <rpadiper@gmail.com>
2022-05-02 19:58:40 -0700

    Revert "Merge branch 'Mohammad_branch'"
    This reverts commit 5c4e75bf499a8c2d89be22f25f1378bffc4fc125, reversing
    changes made to 3d816c9dab8104bbdfaa2868987c1f7b020fa78c.

--- myDigitalMarketPlace/app/routes.py
+++ myDigitalMarketPlace/app/routes.py
@@ -1,35 +1,47 @@
-from app import myapp_obj
+from app import flaskObj
+from flask import render_template
 
-from flask import render_template, flash, request
+# Splash page (Umesh)
+@flaskObj.route('splash')
+def splash():
+    return 'home'
 
-from flask_wtf import FlaskForm
+# Home page (Rafael)
+@flaskObj.route('/')
+def home():
+    return 'home'
 
-from wtforms import FloatField,StringField, IntegerField, BooleanField, SubmitField, validators
- 
-from wtforms.validators import InputRequired, DataRequired, Length
+# Log in (Rafael)
+@flaskObj.route('login')
+def login():
+    return 'home'
 
+# Sign up (Rafael)
+@flaskObj.route('signup')
+def signup():
+    return 'home'
 
-from flask_login import current_user
-from flask_login import login_user
-from flask_login import logout_user
-from flask_login import login_required
+# Profile (Rafael)
+@flaskObj.route('profile')
+def profile():
+    return 'home'
 
+# Cart (Mohammad)
+@flaskObj.route('cart')
+def cart():
+    return 'home'
 
-class PostProduct(FlaskForm):
-	productname= StringField('Product Name' ,validators= [DataRequired(), validators.Length(max=64)])
-	productprice=  FloatField('Product Price' ,  [DataRequired()])
-	productdescription= StringField('Product Description' ,validators= [DataRequired(),validators.Length(max=500)])
-	submit = SubmitField('Post')
-@myapp_obj.route('/PostProduct',methods=['GET','POST'])
-def Product():
-	form= PostProduct()
-	if request.method == 'POST' and form.validate():
-                product = Product(form.productname.data, form.productprice.data, form.productdescription.data)
-               	db_session.add(user)
-                result= 'Your item has been posted'
-                flash('Your item has been posted')
-                return render_template('ListItemForSelling.html', form=form,result=result)
-	if request.method == 'GET' and form.validate():
-		return render_template('ListItemForSelling.html', form=form)
-	else:
-		return render_template('ListItemForSelling.html', form=form)
+# Item Search (Mohammad)
+@flaskObj.route('itemsearch')
+def itemearch():
+    return 'home'
+
+# Item seller (Umesh)
+@flaskObj.route('seller')
+def seller():
+    return 'home'
+
+# Item rating (Umesh)
+@flaskObj.route('rating')
+def cart():
+    return 'home'
--- myDigitalMarketPlace/run.py
+++ myDigitalMarketPlace/run.py
@@ -1,4 +1,3 @@
-from app import myapp_obj
+from app import flaskObj
 
-myapp_obj.run()
-
+flaskObj.run()
