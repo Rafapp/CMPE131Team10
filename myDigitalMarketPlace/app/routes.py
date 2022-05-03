b5f125935dd3845e73f4f76bf8e886b494f44518
Rafa <rpadiper@gmail.com>
2022-05-02 22:01:14 -0700

    Added signup page
    Created signup page along with logo, now going to add database funcitonality for storing emails and passwords.

--- myDigitalMarketPlace/app/routes.py
+++ myDigitalMarketPlace/app/routes.py
@@ -1,71 +1,20 @@
 from app import flaskObj
 from flask import render_template
 
-<<<<<<< Updated upstream
-# Splash page (Umesh)
-@flaskObj.route('splash')
-def splash():
-    return 'home'
-
 # Home page (Rafael)
-@flaskObj.route('/')
-def home():
-    return 'home'
-
-# Log in (Rafael)
-@flaskObj.route('login')
-def login():
-    return 'home'
-
-# Sign up (Rafael)
-@flaskObj.route('signup')
-def signup():
-    return 'home'
-
-# Profile (Rafael)
-@flaskObj.route('profile')
-def profile():
-    return 'home'
-
-# Cart (Mohammad)
-@flaskObj.route('cart')
-def cart():
-    return 'home'
-
-# Item Search (Mohammad)
-@flaskObj.route('itemsearch')
-def itemearch():
-    return 'home'
-
-# Item seller (Umesh)
-@flaskObj.route('seller')
-def seller():
-    return 'home'
-
-# Item rating (Umesh)
-@flaskObj.route('rating')
-def cart():
-    return 'home'
-=======
-# Home page (Rafael) DONE!
 @flaskObj.route('/')
 def home():
     return render_template("Home.html")
 
-# Splash page (Umesh)
-@flaskObj.route('/splash')
-def splash():
-    return 'splash'
-
 # Log in (Rafael)
 @flaskObj.route('/login')
 def login():
-    return 'login'
+    return 'log in'
 
 # Sign up (Rafael)
 @flaskObj.route('/signup')
 def signup():
-    return render_template("Signup.html")
+    return render_template("signup.html")
 
 # Profile (Rafael)
 @flaskObj.route('/profile')
@@ -80,7 +29,7 @@ def cart():
 # Item Search (Mohammad)
 @flaskObj.route('/itemsearch')
 def itemearch():
-    return 'itemsearch'
+    return 'item search'
 
 # Item seller (Umesh)
 @flaskObj.route('/seller')
@@ -91,4 +40,4 @@ def seller():
 @flaskObj.route('/rating')
 def rating():
     return 'rating'
->>>>>>> Stashed changes
+
