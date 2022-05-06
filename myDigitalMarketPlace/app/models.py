ae530ed6ea289ef21e20d91f9db01bb79f88b914
Mohammad-Rahhal <57151103+Mohammad-Rahhal@users.noreply.github.com>
2022-05-06 15:58:32 -0700

    Updated models to main

--- myDigitalMarketPlace/app/models.py
+++ myDigitalMarketPlace/app/models.py
@@ -2,27 +2,6 @@ from app import db
 from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-<<<<<<< Updated upstream
-
-
-class Product1(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    productname = db.Column(db.String(64),unique = False)
-    productprice = db.Column(db.Float,unique = False)
-    productdescription = db.Column(db.String(500),unique=False)
-    productimage = db.Column(db.String(500),unique=False)
-    def __repr__(self):
-        return f'<Product: {self.productname}>'
-
-class Cart(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    productname = db.Column(db.String(64),unique = False)
-    productprice = db.Column(db.Float,unique = False)
-    productimage = db.Column(db.String(500),unique=False)
-    def __repr__(self):
-        return f'<Product: {self.productname}>'
-
-=======
  
  
 class Product1(UserMixin, db.Model):
@@ -41,4 +20,3 @@ class Cart(UserMixin, db.Model):
    productimage = db.Column(db.String(500),unique=False)
    def __repr__(self):
        return f'<Product: {self.productname}>'
->>>>>>> Stashed changes
