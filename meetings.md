c4753a7f07084bcb3a24d071633f8a4982da804f
Rafa <rpadiper@gmail.com>
2022-05-06 22:37:32 -0700

    This is mohammed's work, i helped him implement it

--- meetings.md
+++ meetings.md
@@ -1,6 +1,4 @@
 ## Meetings
-
-
 ### 2022-04-15 at 13:30
 - Umesh, Mohammad and Rafael attended the meeting
 - Updates: Team decided on all requiremenents, got contact info, distributed work equally and prepared first deliverable
@@ -8,24 +6,27 @@
 - Rafael: Created github repository, added teammates, discussed the expectations from the app, created discord server, will work on 2 of the 6 system requirements
 - Mohammad: Helped decide on requirements, discussed the expectations from the app, prepared github and cloned repo, will work on 2 of the 6 system requirements
 - No pair programming this week.
-
-### 2022-04-29 at 6:00
-- Mohammad and Rafael attended the meeting
-- Updates: Delegated work, got started with branches
-- Rafael: Helped Mohammad navigate the repository, and tested branching and merging
-- No pair programming this week.
-
-### 2022-04-27 at 17:30
-<meeting template would go here>
-<only fill in template once you had the meeting>
-
-### 2022-04-29 at 10:30
-- Who attended the meeting
-- Team updates (15min or less)
-  <Each team member gives an update of what they did from the last meeting and what they plan to do next. If they are stuck, bring it up here to see if others can help.>
-  - <name> worked on X. Will work on Y next. 
-
-- pair-programming (optional) (25 mins)
-  <- Pair program on one part of the project. Driver will be the person responsible for that requirement.>
-
-- Summarize group progress
+ 
+### 2022-04-22 at 13:30
+- Umesh, Mohammad and Rafael attended the meeting
+- Updates: Team decided which functional requirements we will do for the MVP.
+- Mohammad: will do the search backend, and the post product front end and backend, and adding images to items.
+- Rafael: will do the the login, logout, user page, and Signup
+- Umesh: will do the splash page, item rating frontend and backend
+ 
+ 
+ 
+### 2022-05-5 at 20:30
+- Mohammad, Umesh, Rafael attended the meeting
+- Updates: Each member presented what they did and explained how they will move forward.
+  - Mohammad: finished the Postproduct functions and database, was working on the search backend
+ - Umesh: finished the rating frontend, is working on the rating backend and the splash page.
+ - Rafael: finished the frontend for the first four functions, is currently working on the login database.
+ 
+ 
+### 2022-05-6 at 21:30
+- Mohammad, Umesh, Rafael attended the meeting
+- Updates: We all put our codes together and made sure it worked
+  - Mohammad: merged the functional requirements
+ - Umesh: merged the functional requirements
+ - Rafael: merged the functional requirements
--- myDigitalMarketPlace/app/routes.py
+++ myDigitalMarketPlace/app/routes.py
@@ -1,5 +1,36 @@
 from app import flaskObj
 from flask import render_template
+from sqlalchemy.orm import Session
+from flask import  flash,request, redirect
+from flask_wtf import FlaskForm
+from wtforms import FloatField,StringField, IntegerField, BooleanField, SubmitField, validators
+from wtforms.validators import InputRequired, DataRequired, Length
+from app import db
+from app.models import Product1, Cart
+from flask_login import current_user
+from flask_login import login_user
+from flask_login import logout_user
+from flask_login import login_required
+db.create_all()
+
+# class for linking html to product database
+class PostProduct(FlaskForm):
+  productname= StringField('Product Name' ,validators= [DataRequired(), validators.Length(max=64)])
+  productprice=  FloatField('Product Price' ,  [DataRequired()])
+  productdescription= StringField('Product Description' ,validators= [DataRequired(),validators.Length(max=500)])
+  productimage = StringField('Image Link',validators = [validators.Length(max=500)])
+  submit = SubmitField('Post')
+  home = SubmitField('Home')
+
+ # class for putting searches in the html
+class SearchClass(FlaskForm):
+  search = SubmitField('Search')
+  searchbox = StringField("searchbox",validators = [DataRequired()])
+ 
+class AddtoCart(FlaskForm):
+   addtocart = SubmitField('Add to cart')
+
+
 
 # Home page (Rafael)
 @flaskObj.route('/')
@@ -22,14 +53,68 @@ def profile():
     return render_template("Profile.html")
 
 # Cart (Mohammad)
+# adds whatever is pressed on to the Cart database
 @flaskObj.route('/cart')
 def cart():
-    return 'cart'
+   product_id = request.form.get('product_id')
+   product= Product1.query.filter_by(id=product_id).first()
+   if request.method =="POST":
+       u = Cart(productname= product.productname, productprice = product.productprice, productimage=product.productimage)
+       db.session.add(u)
+       db.session.commit()
+   return render_template('cart.html')
 
 # Item Search (Mohammad)
-@flaskObj.route('/itemsearch')
-def itemearch():
-    return 'item search'
+ 
+#Confirms to customer that their product has been posted then allows them to go to home
+@flaskObj.route('/inbetween',methods=['GET','POST'])
+def inbetween():
+  form= PostProduct()
+  if request.method == 'GET':
+      submit = SubmitField('Home')
+      return render_template('inbetween.html', home=submit,form=form)
+  if request.method == 'POST':
+      return redirect('/')
+
+@flaskObj.route('/itemgallery',methods=['GET','POST'])
+def Home():
+  form=SearchClass()
+  if request.method == 'GET':
+      return render_template('itemgallery.html', form=form)
+  if request.method == 'POST':
+      return redirect('/search')
+
+#When search is clickled, the searched phrase is compared to the products database to display all matches
+@flaskObj.route('/search',methods=['GET','POST'])
+def searchresult():
+    form= SearchClass()
+    cart= AddtoCart()
+    list = []
+    searchedphrase = Product1.query.all()
+    if form.validate_on_submit():
+        #if request.method == 'POST':
+        result = request.form['searchbox']
+        product = Product1.query.filter(Product1.productname == result).first()
+        #searchedphrase = Product1.query.filter_by(productname = result).first()
+    for i in searchedphrase:
+        if (i.productname==result):
+            list.append(i.productname)
+        return render_template('search.html',result=result,form=form,searchedphrase=searchedphrase,product=product,list=list,cart=cart)
+    #return redirect('/searchhelp')
+    return render_template('search.html',form=form,)
+
+#Saves whatever is inputed into the products databse
+@flaskObj.route('/PostProduct',methods=['GET','POST'])
+def Product():
+  form= PostProduct()
+  if request.method == 'GET':
+      return render_template('ListItemForSelling.html', form=form)
+  if request.method == 'POST':
+      product = Product1(productname=form.productname.data,productprice= form.productprice.data,productdescription= form.productdescription.data,productimage=form.productimage.data)
+      db.session.add(product)
+      db.session.commit()
+      return redirect('/inbetween')
+
 
 # Item seller (Umesh)
 @flaskObj.route('/seller')
